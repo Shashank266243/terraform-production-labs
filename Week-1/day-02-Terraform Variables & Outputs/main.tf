b868c9250a3425508f05c56564f954e0eb8082e5
Shashank266243@gamil.com <Shashank266243@gamil.com>
2026-07-07 01:48:42 +0530

    second day practice file merge

---
+++ Week-1/day-02-Terraform Variables & Outputs/main.tf
@@ -0,0 +1,12 @@
+resource "azurerm_storage_account" "terraform_SA" {
+  depends_on               = [azurerm_resource_group.st_rg]
+  name                     = var.storage_account_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
---
+++ Week-1/day-02-Terraform Variables & Outputs/terraform.tfvars
@@ -0,0 +1,3 @@
+storage_account_name = "stsa266243"
+resource_group_name  = "St_rg11"
+location             = "centralindia"
